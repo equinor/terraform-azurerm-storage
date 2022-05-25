b36088c8c94e09a7a759e0f54f5bcc32c009e887
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-05-25 14:04:32 +0200

    refactor: add default values for arguments (#42)

--- main.tf
+++ main.tf
@@ -53,6 +53,7 @@ resource "azurerm_storage_container" "this" {
   name                  = each.value
   storage_account_name  = azurerm_storage_account.this.name
   container_access_type = "private"
+  metadata              = {}
 }
 
 resource "azurerm_storage_queue" "this" {
@@ -60,6 +61,7 @@ resource "azurerm_storage_queue" "this" {
 
   name                 = each.value
   storage_account_name = azurerm_storage_account.this.name
+  metadata             = {}
 }
 
 resource "azurerm_storage_table" "this" {
@@ -75,6 +77,7 @@ resource "azurerm_storage_share" "this" {
   name                 = each.value
   storage_account_name = azurerm_storage_account.this.name
   quota                = 5120
+  metadata             = {}
 }
 
 resource "azurerm_monitor_diagnostic_setting" "this" {
@@ -107,6 +110,26 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
   log {
     category = "StorageDelete"
     enabled  = true
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "Capacity"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  metric {
+    category = "Transaction"
+    enabled  = false
 
     retention_policy {
       days    = 0
