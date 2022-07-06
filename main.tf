779a531ce2f52d962c2f3f4c432b1cacf5911e2a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-07-06 08:51:14 +0200

    docs(readme): remove usage example

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-locals {
-  suffix       = "${var.application}-${var.environment}"
-  suffix_alnum = replace(lower(local.suffix), "/[^a-z0-9]+/", "")
-  tags         = merge({ application = var.application, environment = var.environment }, var.tags)
-}
-
 resource "azurerm_storage_account" "this" {
-  name                = coalesce(var.account_name, "st${local.suffix_alnum}")
+  name                = coalesce(var.account_name, replace(lower("st${var.application}${var.environment}"), "/[^a-z0-9]+/", ""))
   resource_group_name = var.resource_group_name
   location            = var.location
 
@@ -19,7 +13,7 @@ resource "azurerm_storage_account" "this" {
   shared_access_key_enabled       = var.shared_access_key_enabled
   allow_nested_items_to_be_public = var.allow_blob_public_access
 
-  tags = local.tags
+  tags = merge({ application = var.application, environment = var.environment }, var.tags)
 
   blob_properties {
     versioning_enabled  = var.blob_versioning_enabled
