0f25f8c5312518d4905bf640191eea487016cedd
sebastianlolv <sebastian@lyse.net>
2022-08-17 14:14:03 +0200

    remove module folder off object replication and instead create example version

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -47,3 +47,68 @@ resource "azurerm_role_assignment" "this" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+# Example of Object Replication
+
+resource "azurerm_resource_group" "src" {
+  name     = "rg-src-${var.application}-${var.environment}"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "src" {
+  name                = "st${var.application}${var.environment}"
+  resource_group_name = azurerm_resource_group.src.name
+  location            = azurerm_resource_group.src.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = true
+  }
+
+
+}
+
+resource "azurerm_storage_container" "src" {
+  name                  = "ci${var.application}${var.environment}"
+  storage_account_name  = azurerm_storage_account.src.name
+  container_access_type = "private"
+}
+
+resource "azurerm_resource_group" "dst" {
+  name     = "rg-dst-${var.application}${var.environment}"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "dst" {
+  name                = "st${var.application}${var.environment}"
+  resource_group_name = azurerm_resource_group.dst.name
+  location            = azurerm_resource_group.dst.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    versioning_enabled  = true
+    change_feed_enabled = true
+  }
+}
+
+resource "azurerm_storage_container" "dst" {
+  name                  = "ci${var.application}${var.environment}"
+  storage_account_name  = azurerm_storage_account.dst.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_object_replication" "example" {
+  source_storage_account_id      = azurerm_storage_account.src.id
+  destination_storage_account_id = azurerm_storage_account.dst.id
+
+  rules {
+    source_container_name      = azurerm_storage_account.src.name
+    destination_container_name = azurerm_storage_container.dst.name
+  }
+}
+
