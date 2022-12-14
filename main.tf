c6a1e0e223fe4935b0f806011cd72c7ecc4b4545
sebastianlolv <sebastian@lyse.net>
2022-12-14 15:30:11 +0100

    add logging for queue service requests

--- main.tf
+++ main.tf
@@ -34,6 +34,28 @@ resource "azurerm_storage_account" "this" {
     }
   }
 
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+  }
+
   network_rules {
     default_action = length(var.firewall_ip_rules) == 0 ? "Allow" : "Deny"
     bypass         = ["AzureServices"]
