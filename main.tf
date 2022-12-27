b562c680edc1a1c28a9fa65ea8429052e4d7c9cd
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-12-27 13:46:00 +0100

    fix: correct ref to var

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_storage_account" "this" {
 
   network_rules {
     default_action             = var.firewall_default_action
-    bypass                     = var.firewall.bypass
+    bypass                     = var.firewall_bypass
     ip_rules                   = var.firewall_ip_rules
     virtual_network_subnet_ids = var.firewall_virtual_network_subnet_ids
   }
