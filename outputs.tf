de80c0e8d16bf13bfaf01d69018c45ac2453546d
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-03-29 12:56:37 +0200

    feat: add output for system assigned identity principal id

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "account_name" {
   value       = azurerm_storage_account.this.name
 }
 
-output "principal_id" {
+output "identity_principal_id" {
   description = "The Principal ID for the Service Principal associated with the Identity of this Storage Account."
   value       = azurerm_storage_account.this.identity.0.principal_id
 }
