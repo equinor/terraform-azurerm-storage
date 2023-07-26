8d94fed5fc197d64cfbe7cd2ad5e31d117cda612
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-07-26 09:21:34 +0200

    fix: don't specify Log Analytics destination type (#161)

--- main.tf
+++ main.tf
@@ -176,10 +176,12 @@ resource "azurerm_advanced_threat_protection" "this" {
 resource "azurerm_monitor_diagnostic_setting" "this" {
   for_each = toset(["blob", "queue", "table", "file"])
 
-  name                           = "audit-logs"
-  target_resource_id             = "${azurerm_storage_account.this.id}/${each.value}Services/default"
-  log_analytics_workspace_id     = var.log_analytics_workspace_id
-  log_analytics_destination_type = var.log_analytics_destination_type
+  name                       = "audit-logs"
+  target_resource_id         = "${azurerm_storage_account.this.id}/${each.value}Services/default"
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  # Ref: https://registry.terraform.io/providers/hashicorp/azurerm/3.65.0/docs/resources/monitor_diagnostic_setting#log_analytics_destination_type
+  log_analytics_destination_type = null
 
   dynamic "enabled_log" {
     for_each = toset(var.diagnostic_setting_enabled_log_categories)
