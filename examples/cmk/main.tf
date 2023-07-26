7b42e44ade80330e76ec09588e8b7e16fcba716f
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-07-26 12:45:38 +0200

    feat!: enforce blob properties (#169)
    * feat!: enforce blob properties
    Fixes #162
    
    * chore: update CMK example

--- examples/cmk/main.tf
+++ examples/cmk/main.tf
@@ -37,15 +37,6 @@ module "storage" {
   account_replication_type = "GRS"
 
   system_assigned_identity_enabled = true
-
-  blob_properties = {
-    change_feed_enabled = false
-    versioning_enabled  = false
-    restore_policy_days = 0
-  }
-
-  share_properties = null
-  queue_properties = null
 }
 
 resource "azurerm_storage_account_customer_managed_key" "ok_cmk" {
