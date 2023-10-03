289f1f2b243f5063020fac7a34778e5e08eeca2b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-10-03 10:23:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ locals {
   is_standard_data_lake_storage = var.account_tier == "Standard" && var.account_kind == "StorageV2" && var.is_hns_enabled
   # No need to check for "is_standard_gpv2_storage", since that is what this module is configured for by default.
 
-  # Storage account network IP rules only support 0-30 number of bits as prefix.
-  # If number of bits is >30, remove prefix.
-  network_rules_ip_rules = [
-    for ip_rule in var.network_rules_ip_rules : split("/", ip_rule)[1] > 30 ? split("/", ip_rule)[0] : ip_rule
-  ]
+  # Storage network IP rules only support 0-30 number of bits as prefix.
+  # If number of bits is >30, add host IPs in range to list.
+  network_rules_ip_rules = distinct(concat([
+    for ip_rule in local.key_vault_network_acls_ip_rules : split("/", ip_rule)[1] > 30 ? compact([cidrhost(ip_rule, 0), try(cidrhost(ip_rule, 1), null)]) : [ip_rule]
+  ]...))
 
   # If system_assigned_identity_enabled is true, value is "SystemAssigned".
   # If identity_ids is non-empty, value is "UserAssigned".
