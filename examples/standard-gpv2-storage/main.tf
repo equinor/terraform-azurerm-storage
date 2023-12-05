a618f2d5ad6cef013c798730db749557ad0f9edf
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-05 08:57:49 +0100

    docs: development and testing

--- examples/standard-gpv2-storage/main.tf
+++ examples/standard-gpv2-storage/main.tf
@@ -8,17 +8,12 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-${random_id.this.hex}"
-  location = var.location
-}
-
 module "log_analytics" {
   source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.3.0"
 
   workspace_name      = "log-${random_id.this.hex}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "storage" {
@@ -26,7 +21,7 @@ module "storage" {
   source = "../.."
 
   account_name               = "st${random_id.this.hex}"
-  resource_group_name        = azurerm_resource_group.this.name
-  location                   = azurerm_resource_group.this.location
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
   log_analytics_workspace_id = module.log_analytics.workspace_id
 }
